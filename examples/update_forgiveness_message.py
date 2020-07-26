29c2c3727eb29f8d39f2af0ff408844671fb0ed7
UsSbaForgivenessDev <developer@ussbaforgiveness.com>
2020-07-26 22:07:42 +0530

    Updated the latest fixes

--- examples/update_forgiveness_message.py
+++ examples/update_forgiveness_message.py
@@ -3,13 +3,13 @@ from PPPForgivenessSDK.client import Client
 # to run file 'read_forgiveness_message.py', use valid token and slug associated with a valid thread
 client = Client(
     access_token='{{YOUR_TOKEN_HERE}}',
-    vendor_key='{{YOUR_VENDOR_KEY}}'.
+    vendor_key='{{YOUR_VENDOR_KEY}}',
     environment='sandbox'
 )
 
 message_api = client.messages
 
-result = message_api.update('{{SLUG_HERE}}', int('{{DOCUMENT_TYPE_ID_HERE}}'),'{{DOCUMENT_NAME}}', '{{PATH_TO_DOCUMENT_FILE_HERE}}', '')
+result = message_api.update('{{SLUG_HERE}}', int('{{DOCUMENT_TYPE_ID_HERE}}'),'{{DOCUMENT_NAME}}', '{{PATH_TO_DOCUMENT_FILE_HERE}}', '{{MESSAGE}}')
 
 
 if result['status'] == 200:
